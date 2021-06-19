e4dd9b1d4d25802ce29cd2ec043e74c178c82159
Khinenw <deu07115@gmail.com>
2021-06-20 03:26:43 +0900

    style: fix indentation

--- app/components/KdThemeButton.vue
+++ app/components/KdThemeButton.vue
@@ -1,62 +1,62 @@
 <template>
-  <div @click="change" class="KdThemeButton__button">
-    <div class="KdThemeButton__container">
-		  <icon-LightMode v-if="isDark" class="KdThemeButton__icon" />
-		  <icon-DarkMode v-if="isLight" class="KdThemeButton__icon" />
-    
-      <div class="KdThemeButton__text" v-if="isDark">{{$t('lightmode')}}</div>
-      <div class="KdThemeButton__text" v-if="isLight">{{$t('darkmode')}}</div>
-    </div>
-  </div>
+	<div @click="change" class="KdThemeButton__button">
+		<div class="KdThemeButton__container">
+			<icon-LightMode v-if="isDark" class="KdThemeButton__icon" />
+			<icon-DarkMode v-if="isLight" class="KdThemeButton__icon" />
+
+			<div class="KdThemeButton__text" v-if="isDark">{{$t('lightmode')}}</div>
+			<div class="KdThemeButton__text" v-if="isLight">{{$t('darkmode')}}</div>
+		</div>
+	</div>
 </template>
 
 <i18n>
 {
 	"ko": {
-    "lightmode": "밝은 테마",
+	"lightmode": "밝은 테마",
 		"darkmode": "어두운 테마"
 	},
 
 	"en": {
-    "lightmode": "Light Theme",
+	"lightmode": "Light Theme",
 		"darkmode": "Dark Theme"
 	},
 
 	"ja": {
-    "lightmode": "明るいテーマ",
+	"lightmode": "明るいテーマ",
 		"darkmode": "暗いテーマ"
 	}
 }
 </i18n>
 
 <style lang="less" scoped>
-  .KdThemeButton__button {
+	.KdThemeButton__button {
 		width: 30vw;
 		max-width: 230px;
 
-    cursor: pointer;
+		cursor: pointer;
 
 		@media (max-width: 768px) {
 			width: 65vw;
 		}
-  }
-  
-  .KdThemeButton__container {
+	}
+
+	.KdThemeButton__container {
 		font-family: var(--font-sans);
 		font-size: 1.05rem;
 		font-weight: 500;
 
-    padding: 10px 20px;
+		padding: 10px 20px;
 		text-overflow: ellipsis;
 		text-decoration: none;
 		overflow: hidden;
-    white-space: nowrap;
+		white-space: nowrap;
 
-    display: flex;
-    flex: row;
-    justify-content: center;
-    align-items: center;
-    
+		display: flex;
+		flex: row;
+		justify-content: center;
+		align-items: center;
+
 		background: var(--grey-050_rgb);
 		color: var(--grey-900_rgb);
 		transition: background .4s ease;
@@ -64,48 +64,48 @@
 		&:hover {
 			background: var(--grey-100_rgb);
 		}
-  }
+	}
 
-  .KdThemeButton__icon {
-    width: 16px;
-    height: 16px;
+	.KdThemeButton__icon {
+		width: 16px;
+		height: 16px;
 
-    font-size: 0;
-  }
+		font-size: 0;
+	}
 
-  .KdThemeButton__text {
-    margin-left: 8px;
-  }
+	.KdThemeButton__text {
+		margin-left: 8px;
+	}
 </style>
 
 <script>
 	import IconLightMode from "@/images/IconLightMode?inline";
 	import IconDarkMode from "@/images/IconDarkMode?inline";
 
-  export default {
-    computed: {
-      theme() {
-        return this.$store.state.theme.theme;
-      },
+	export default {
+		computed: {
+			theme() {
+				return this.$store.state.theme.theme;
+			},
 
-      isDark() {
-        return this.$store.state.theme.theme === 'dark';
-      },
+			isDark() {
+				return this.$store.state.theme.theme === 'dark';
+			},
 
-      isLight() {
-        return this.$store.state.theme.theme === 'light';
-      }
-    },
+			isLight() {
+				return this.$store.state.theme.theme === 'light';
+			}
+		},
 
-    methods: {
-      change() {
-        this.$store.commit('theme/setIsAnimation', true);
-      }
-    },
+		methods: {
+			change() {
+				this.$store.commit('theme/setIsAnimation', true);
+			}
+		},
 
-    components: {
-      IconLightMode,
-      IconDarkMode
-    }
-  }
+		components: {
+			IconLightMode,
+			IconDarkMode
+		}
+	}
 </script>
--- app/layouts/KdThemeOverlay.vue
+++ app/layouts/KdThemeOverlay.vue
@@ -1,105 +1,105 @@
 <template>
-  <transition name="fade" mode="out-in">
-    <div v-if="isAnimation" class="KdThemeOverlay">
-      <transition name="slide" mode="out-in">
-        <icon-LightMode v-if="isLight" class="KdThemeOverlay__icon" />
-      </transition>
-      <transition name="slide" mode="out-in">
-        <icon-DarkMode v-if="isDark" class="KdThemeOverlay__icon" />
-      </transition>
-    </div>
-  </transition>
+	<transition name="fade" mode="out-in">
+		<div v-if="isAnimation" class="KdThemeOverlay">
+			<transition name="slide" mode="out-in">
+				<icon-LightMode v-if="isLight" class="KdThemeOverlay__icon" />
+			</transition>
+			<transition name="slide" mode="out-in">
+				<icon-DarkMode v-if="isDark" class="KdThemeOverlay__icon" />
+			</transition>
+		</div>
+	</transition>
 </template>
 
 <style lang="less" scoped>
-  .KdThemeOverlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+	.KdThemeOverlay {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 10000;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		z-index: 10000;
 
-    background: var(--grey-900_rgb);
+		background: var(--grey-900_rgb);
 
-    transition: background .75s;
-  }
+		transition: background .75s;
+	}
 
-  .KdThemeOverlay__icon {
-    color: var(--grey-050_rgb);
+	.KdThemeOverlay__icon {
+		color: var(--grey-050_rgb);
 
-    position: absolute;
-    width: 128px;
-    height: 128px;
+		position: absolute;
+		width: 128px;
+		height: 128px;
 
-    transition: color .75s;
-  }
+		transition: color .75s;
+	}
 
-  .fade-enter-active, .fade-leave-active {
-    transition: all .75s;
-  }
+	.fade-enter-active, .fade-leave-active {
+		transition: all .75s;
+	}
 
-  .fade-enter, .fade-leave-to {
-    opacity: 0;
-  }
+	.fade-enter, .fade-leave-to {
+		opacity: 0;
+	}
 
-  .slide-enter-active, .slide-leave-active {
-    transition: all .75s;
-  }
+	.slide-enter-active, .slide-leave-active {
+		transition: all .75s;
+	}
 
-  .slide-enter {
-    transform: translateX(100%);
-    opacity: 0;
-  }
+	.slide-enter {
+		transform: translateX(100%);
+		opacity: 0;
+	}
 
-  .slide-leave-to {
-    transform: translateX(-100%);
-    opacity: 0;
-  }
+	.slide-leave-to {
+		transform: translateX(-100%);
+		opacity: 0;
+	}
 </style>
 
 <script>
-  import IconLightMode from "@/images/IconLightMode?inline";
-  import IconDarkMode from "@/images/IconDarkMode?inline";
+	import IconLightMode from "@/images/IconLightMode?inline";
+	import IconDarkMode from "@/images/IconDarkMode?inline";
 
-  export default {
-    computed: {
-      isAnimation() {
-        return this.$store.state.theme.isAnimation;
-      },
+	export default {
+		computed: {
+			isAnimation() {
+				return this.$store.state.theme.isAnimation;
+			},
 
-      isDark() {
-        return this.$store.state.theme.theme === "dark";
-      },
+			isDark() {
+				return this.$store.state.theme.theme === "dark";
+			},
 
-      isLight() {
-        return this.$store.state.theme.theme === "light";
-      },
-    },
+			isLight() {
+				return this.$store.state.theme.theme === "light";
+			},
+		},
 
-    watch: {
-      isAnimation(newState) {
-        if (newState) {
-          setTimeout(() => {
-            if (this.$store.state.theme.theme === "dark") {
-              this.$store.dispatch("theme/changeTheme", "light");
-            } else {
-              this.$store.dispatch("theme/changeTheme", "dark");
-            }
+		watch: {
+			isAnimation(newState) {
+				if (newState) {
+					setTimeout(() => {
+						if (this.$store.state.theme.theme === "dark") {
+							this.$store.dispatch("theme/changeTheme", "light");
+						} else {
+							this.$store.dispatch("theme/changeTheme", "dark");
+						}
 
-            setTimeout(() => this.$store.commit('theme/setIsAnimation', false), 750);
-          }, 750);
-        }
-      },
-    },
+						setTimeout(() => this.$store.commit('theme/setIsAnimation', false), 750);
+					}, 750);
+				}
+			},
+		},
 
-    components: {
-      IconLightMode,
-      IconDarkMode,
-    },
-  };
+		components: {
+			IconLightMode,
+			IconDarkMode,
+		},
+	};
 </script>
